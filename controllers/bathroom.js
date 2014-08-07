e2e7c391dfab36ced15985c5f099c9e5df337a4e
Peter Henderson <peter@athinkingape.com>
2014-08-07 13:34:38 -0700

    stats

--- controllers/bathroom.js
+++ controllers/bathroom.js
@@ -6,9 +6,9 @@ var BathroomStatus = require('../models/BathroomStatus');
 
 exports.occupied = function(req, res) {
   BathroomStatus.findOne({br_id : req.query.bathroom_id}, function(err, br){
+    console.log(br);
   	br.in_use = true;
   	br.start_time = new Date();
-  	br.number_of_uses += 1;
   	br.save()
   });
 	res.status(200).end()
@@ -16,8 +16,14 @@ exports.occupied = function(req, res) {
 
 exports.unoccupied = function(req, res) {
   BathroomStatus.findOne({br_id : req.query.bathroom_id}, function(err, br){
+    console.log(br);
   	br.in_use = false;
   	br.uses.push({start_time : br.start_time, end_time : new Date()})
+    var summ = 0;
+    for(var i = 0; i < br.uses.length; i++){
+      summ += Math.round((br.uses[i].end_time - br.uses[i].start_time)/1000);
+    }
+    br.average = summ/br.uses.length;
   	br.save()
   });
 	res.status(200).end()
--- models/BathroomStatus.js
+++ models/BathroomStatus.js
@@ -4,10 +4,13 @@ var bathroomStatusSchema = new mongoose.Schema({
   br_id : Number, // 1,2,3,4 for the bathrooms we have in use
   in_use : Boolean,
   start_time : Date, //Of use
+  average : Number,
+  count: Number,
   uses : [{
     start_time : Date,
     end_time : Date
-  }]
+  }],
+  ratings : [Number]
 });
 
 module.exports = mongoose.model('BathroomStatus', bathroomStatusSchema);
